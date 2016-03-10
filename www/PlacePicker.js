c2a7aaaec6465557777a4ec7cd7979f5f9fb3ebc
jvdheijden92 <jvdheijden92@gmail.com>
2016-03-10 15:20:22 +0100

    Update PlacePicker.js

--- www/PlacePicker.js
+++ www/PlacePicker.js
@@ -1,35 +1,35 @@
 /**
-     * Constructor
-     */
-    function PlacePicker() {
-      //this._callback;
-    }
+ * Constructor
+ */
+function PlacePicker() {
+  //this._callback;
+}
 
-    /**
-     * show - true to show the ad, false to hide the ad
-     */
-    PlacePicker.prototype.show = function(cb, errCb) {
-        cordova.exec(cb, errCb, "PlacePickerPlugin", '', []);
-    };
+/**
+ * show - true to show the ad, false to hide the ad
+ */
+PlacePicker.prototype.show = function(cb, errCb) {
+    cordova.exec(cb, errCb, "PlacePickerPlugin", '', []);
+};
 
-    var placePicker = new PlacePicker();
-    module.exports = placePicker;
+var placePicker = new PlacePicker();
+module.exports = placePicker;
 
-    // Make plugin work under window.plugins
+// Make plugin work under window.plugins
+if (!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.placePicker) {
+    window.plugins.placePicker = placePicker;
+}
+
+PlacePicker.install = function () {
     if (!window.plugins) {
         window.plugins = {};
     }
-    if (!window.plugins.placePicker) {
-        window.plugins.placePicker = placePicker;
-    }
 
-    PlacePicker.install = function () {
-        if (!window.plugins) {
-            window.plugins = {};
-        }
+    window.plugins.placePicker = new PlacePicker();
+    return window.plugins.placePicker;
+};
 
-        window.plugins.placePicker = new PlacePicker();
-        return window.plugins.placePicker;
-    };
-
-    cordova.addConstructor(PlacePicker.install);
+cordova.addConstructor(PlacePicker.install);
