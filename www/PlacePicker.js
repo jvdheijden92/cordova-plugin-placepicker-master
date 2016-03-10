8770e2501fdce905b932eae7d172c048f0f240e2
jvdheijden92 <jvdheijden92@gmail.com>
2016-03-10 15:09:06 +0100

    Update PlacePicker.js

--- www/PlacePicker.js
+++ www/PlacePicker.js
@@ -1,17 +1,35 @@
-function PlacePicker() {}
+/**
+     * Constructor
+     */
+    function PlacePicker() {
+      //this._callback;
+    }
 
-PlacePicker.prototype.open = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "PlacePicker", "", []);
-};
+    /**
+     * show - true to show the ad, false to hide the ad
+     */
+    PlacePicker.prototype.show = function(cb, errCb) {
+        cordova.exec(cb, errCb, "PlacePickerPlugin", '', []);
+    };
 
+    var placePicker = new PlacePicker();
+    module.exports = placePicker;
 
-PlacePicker.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
+    // Make plugin work under window.plugins
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+    if (!window.plugins.placePicker) {
+        window.plugins.placePicker = placePicker;
+    }
 
-  window.plugins.placepicker = new PlacePicker();
-  return window.plugins.placepicker;
-};
+    PlacePicker.install = function () {
+        if (!window.plugins) {
+            window.plugins = {};
+        }
 
-cordova.addConstructor(PlacePicker.install);
+        window.plugins.placePicker = new PlacePicker();
+        return window.plugins.placePicker;
+    };
+
+    cordova.addConstructor(PlacePicker.install);
